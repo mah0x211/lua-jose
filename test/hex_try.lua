04b229a9e843a81b7bedb6d9dd2f7aafa6f15d10
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-03 13:38:04 +0900

    change: hex_try.lua has been updated.

--- test/hex_try.lua
+++ test/hex_try.lua
@@ -1,12 +1,12 @@
-local jose = require('jose.lib');
+local hex = require('jose.hex');
 
 local function dec( src, dest )
-    dest = ifNil( jose.hex.decode( dest ) );
+    dest = ifNil( hex.decode( dest ) );
     ifNotEqual( src, dest );
 end
 
 local function enc( src )
-    dec( src, ifNil( jose.hex.encode( src ) ) );
+    dec( src, ifNil( hex.encode( src ) ) );
 end
 
 enc('{"foo":{"bar":1},"baz":"hello"}');
