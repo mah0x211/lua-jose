6ae9c2f4ac6a7571f49920c2a184c9e9c1a34059
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-03 13:42:59 +0900

    change: buffer_try.lua has been updated.

--- test/buffer_try.lua
+++ test/buffer_try.lua
@@ -1,38 +1,38 @@
-local lib = require('jose.lib');
+local buffer = require('jose.buffer');
 local data = '{"foo":{"bar":1},"baz":"hello"}';
 
-local buf = ifNil( lib.buffer( data ) );
-local hex = ifNil( buf:convert( lib.FMT_HEX ) );
-local b64 = ifNil( buf:convert( lib.FMT_BASE64 ) );
-local b64url = ifNil( buf:convert( lib.FMT_BASE64URL ) );
+local buf = ifNil( buffer.new( data ) );
+local hex = ifNil( buf:toHex() );
+local b64 = ifNil( buf:toBase64() );
+local b64url = ifNil( buf:toBase64URL() );
 
-ifNil( buf:convert() );
-ifNotEqual( buf:convert(), data );
+ifNil( buf );
+ifNotEqual( tostring(buf), data );
 
 local function compare( raw, fmt, hex, b64, b64url )
-    local buf = ifNil( lib.buffer( raw, fmt ) );
+    local buf = ifNil( buffer.new( raw, fmt ) );
     
     ifNotTrue( buf:compare( data ) );
-    ifNotTrue( buf:compare( hex, lib.FMT_HEX ) );
-    ifNotTrue( buf:compare( b64, lib.FMT_BASE64 ) );
-    ifNotTrue( buf:compare( b64, lib.FMT_BASE64URL ) );
-    ifNotTrue( buf:compare( b64url, lib.FMT_BASE64 ) );
-    ifNotTrue( buf:compare( b64url, lib.FMT_BASE64URL ) );
+    ifNotTrue( buf:compare( hex, buffer.FMT_HEX ) );
+    ifNotTrue( buf:compare( b64, buffer.FMT_BASE64 ) );
+    ifNotTrue( buf:compare( b64, buffer.FMT_BASE64URL ) );
+    ifNotTrue( buf:compare( b64url, buffer.FMT_BASE64 ) );
+    ifNotTrue( buf:compare( b64url, buffer.FMT_BASE64URL ) );
     ifNotFalse( buf:compare( '{"foo":{"bar":1},"baz":"hello"|' ) );
 end
 
 for _, v in ipairs({ 
     {
         data = hex,
-        fmt = lib.FMT_HEX
+        fmt = buffer.FMT_HEX
     },
     {
         data = b64,
-        fmt = lib.FMT_BASE64
+        fmt = buffer.FMT_BASE64
     },
     {
         data = b64url,
-        fmt = lib.FMT_BASE64URL
+        fmt = buffer.FMT_BASE64URL
     }
 }) do
     compare( v.data, v.fmt, hex, b64, b64url );
