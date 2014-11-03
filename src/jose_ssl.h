609038c2daf85023fdc34274a3c6d1f3a90c1328
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-03 13:04:16 +0900

    add: jose_ssl.h has been added.

--- src/jose_ssl.h
+++ src/jose_ssl.h
@@ -19,33 +19,61 @@
  *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
  *  DEALINGS IN THE SOFTWARE.
  *
- *  jose.h
+ *  jose_ssl.h
  *  lua-jose
  *
- *  Created by Masatoshi Teruya on 14/10/22.
+ *  Created by Masatoshi Teruya on 14/11/02.
  *
  */
 
-#ifndef ___JOSE_LUA___
-#define ___JOSE_LUA___
+#ifndef ___JOSE_SSL_LUA___
+#define ___JOSE_SSL_LUA___
 
 #include "jose_util.h"
+// openssl
+#include <openssl/ssl.h>
+#include <openssl/evp.h>
+#include <openssl/engine.h>
+#include <openssl/rsa.h>
+#include <openssl/ecdsa.h>
+#include <openssl/evp.h>
+#include <openssl/bn.h>
+#include <openssl/pem.h>
+#include <openssl/sha.h>
+#include <openssl/hmac.h>
+#include <openssl/err.h>
 
-// define module names
-#define JOSE_BUFFER_MT  "jose.buffer"
-#define JOSE_DIGEST_MT  "jose.digest"
-#define JOSE_PKEY_MT    "jose.pkey"
-#define JOSE_RSA_MT     "jose.rsa"
 
-// MARK: define prototypes
-LUALIB_API int luaopen_jose_lib( lua_State *L );
-LUALIB_API int luaopen_jose_buffer( lua_State *L );
-LUALIB_API int luaopen_jose_hex( lua_State *L );
-LUALIB_API int luaopen_jose_base64( lua_State *L );
-LUALIB_API int luaopen_jose_digest( lua_State *L );
-LUALIB_API int luaopen_jose_pkey( lua_State *L );
-LUALIB_API int luaopen_jose_rsa( lua_State *L );
-LUALIB_API int luaopen_jose_generate( lua_State *L );
+// MARK: helper functions
+
+static inline void jose_push_sslerror( lua_State *L )
+{
+    SSL_load_error_strings();
+    lua_pushstring( L, ERR_error_string( ERR_get_error(), NULL ) );
+    ERR_free_strings();
+}
+
+
+// password index = idx + 1
+static inline int jose_getopt_cipher( lua_State *L, int idx,
+                                      const char **name,
+                                      const EVP_CIPHER **ciph,
+                                      const char **pswd, size_t *len )
+{
+    *name = luaL_optstring( L, idx, NULL );
+    if( *name )
+    {
+        *ciph = EVP_get_cipherbyname( *name );
+        if( !*ciph ){
+            return -1;
+        }
+        // password required
+        *pswd = luaL_checklstring( L, idx + 1, len );
+    }
+    
+    return 0;
+}
+
 
 
 #endif
