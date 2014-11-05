fe2c0f1c099498f654041026a5ff8a033070ff82
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-05 13:41:43 +0900

    change: jwt_try.lua has been updated.

--- test/jwt_try.lua
+++ test/jwt_try.lua
@@ -1,15 +1,9 @@
-local util = require('util');
 local base64 = require('jose.base64');
 local jose = require('jose');
 local rsaComponent = {
     n = "s8xGhgK4RowoGq4SPcSVU4JREXnMctqXZ5OrMH3bYTGZbMm0pvkR8X9QkrwAwlQEKTy6FxD2CyxoRtKzyFrlaukrwh7oZ-oaTbUNcVnEwDuCLoRWtipz9exCrqibr67LN-Zv27h0Si5MSQcZNa-xi3EB4TyqEVClTz9abg1D80c",
     e = "AQAB",
-    d = "IYcdO440SMuICvmc4zHOWsU5UtXwnxZOF3189czyZNx37MZsTpqxkuQX5VTzm0lJgVsWpIiAKTM9ur890UXJ8-ZH-NtNk7Lbfuy3EkHpVaoL1MTjKiDKAHc0gCHgzXOol_3H6P86aOa21H5AZtTE-HXca1cmH2vL8FKBUmwhHik",
-    p = "76ZK6RfmMWbV8YFWaod3QwTTtnNAcEAxIptq8CjJwRf76iFBpKjPffxQjBWjIL7ZBJ4HwsI8naaIuD_sRX-mlQ",
-    q = "wBCeCox4E5agq4VBhTDEbXNNwoPwUDnRNIyncaBjqsA1S6MxVjCnOHoiOi75XYlzoEzzGqW6udZoNmvI815Haw",
-    dp = "50aVvrP4zC5RRDusuSw41W3AH7DEPFBG2v4gStsiMf5CeGZV4mGjtRMebMKo5n4oFAOqu7MWKpLAGQhfrYaOrQ",
-    dq = "PCa4KFuX26KoDS-9tmXwjsq2xr2ljKqe35NReZ-et0JdLqQcdn7kUCbIA2rkxCuhH8oZtPXEhiElIC4fkECtow",
-    qi = "YTy5XNaF09bkuuH2RWnmSb-LobxCP3K6Viq5Ws54pVGS1-mr-nOQtk5GvhmiA239PO2Jl4Ni4y3gsQj9nXWPWA"
+    d = "IYcdO440SMuICvmc4zHOWsU5UtXwnxZOF3189czyZNx37MZsTpqxkuQX5VTzm0lJgVsWpIiAKTM9ur890UXJ8-ZH-NtNk7Lbfuy3EkHpVaoL1MTjKiDKAHc0gCHgzXOol_3H6P86aOa21H5AZtTE-HXca1cmH2vL8FKBUmwhHik"
 }
 local secret = ifNil( base64.encodeURL('test key') );
 local jws, jwt;
